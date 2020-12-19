9d4ceadb6461ba847a48152fe50a27a4d33fc95a
Timo Hillerns <timo.hillerns@instana.com>
2020-12-19 11:44:22 +0100

    Add ScopedTo capability for Service

--- src/main/java/jenkins/plugins/instana/scope/Service.java
+++ src/main/java/jenkins/plugins/instana/scope/Service.java
@@ -2,6 +2,7 @@ package jenkins.plugins.instana.scope;
 
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
 
 import hudson.Extension;
 import hudson.model.AbstractDescribableImpl;
@@ -11,13 +12,24 @@ public class Service extends AbstractDescribableImpl<Service> {
 
 	private String name;
 
+	private ScopedTo scopedTo;
+
 	@DataBoundConstructor
 	public Service(String name) {
 		this.name = name;
 	}
 
+	@DataBoundSetter
+	public void setScopedTo(ScopedTo scopedTo) {
+		this.scopedTo = scopedTo;
+	}
+
 	public String getName() {
 		return name;
+	}
+
+	public ScopedTo getScopedTo() {
+		return scopedTo;
 	}
 
 	@Symbol("service")
